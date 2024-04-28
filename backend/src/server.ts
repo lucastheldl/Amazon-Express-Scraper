7fcaa9a5a616bbeae2b2cf49e9b1433449d09bb7
lucastheldl <lucastheldl@gmail.com>
2024-04-28 02:26:49 -0300

    initial commit: backend

---
+++ backend/src/server.ts
@@ -0,0 +1,87 @@
+import axios from "axios";
+import express from "express";
+import jsdom from "jsdom";
+
+const app = express();
+const { JSDOM } = jsdom;
+
+interface ProductType {
+  title: string;
+  price: string;
+  reviewAmount: number;
+  amountOfStars: string;
+  imgUrl: string;
+}
+
+app.get("/search", async (req, res) => {
+  const { q } = req.query;
+  const productList: ProductType[] = [];
+
+  await axios({
+    method: "get",
+    url: `https://www.amazon.com/s?k=${q}`,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36",
+      Accept:
+        "text/html, application/xhtml+xml, application/xml;q=0.9, image/webp, */*;q=0.8",
+    },
+  })
+    .then((response) => {
+      const dom = new JSDOM(response.data);
+
+      dom.window.document
+        .querySelectorAll('[data-component-type="s-search-result"]')
+        .forEach((productItem) => {
+          const price = productItem.querySelector(
+            ".a-price .a-offscreen"
+          )?.textContent;
+          const title = productItem.querySelector(
+            ".a-size-mini .a-size-base-plus"
+          )?.textContent;
+          const reviewAmount = productItem.querySelector(
+            '[data-component-type="s-client-side-analytics"] .a-size-base'
+          )?.textContent;
+          const amountOfStars = productItem.querySelector(
+            ".a-icon .a-icon-alt"
+          )?.textContent;
+          const imgUrl =
+            productItem.querySelector<HTMLImageElement>("img.s-image")?.src;
+
+          const product: ProductType = {
+            title: "",
+            price: "",
+            reviewAmount: 0,
+            amountOfStars: "",
+            imgUrl: "",
+          };
+          if (price) {
+            product.price = price;
+          }
+          if (title) {
+            product.title = title;
+          }
+          if (reviewAmount) {
+            product.reviewAmount = Number(reviewAmount);
+          }
+          if (amountOfStars) {
+            product.amountOfStars = amountOfStars;
+          }
+          if (imgUrl) {
+            product.imgUrl = imgUrl;
+          }
+
+          productList.push(product);
+        });
+    })
+    .catch((error) => {
+      console.log("Error:" + error.response.data);
+    });
+
+  res.status(200).json(productList);
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log("Http Server Running");
+});
