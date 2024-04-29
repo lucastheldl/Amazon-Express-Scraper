ff334bf23716f89a98f06eac82c4cd40fc618dad
lucastheldl <lucastheldl@gmail.com>
2024-04-29 13:16:00 -0300

    doc: write comments onfront end js file

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,32 @@
 async function fetchResults() {
+  //Create a list of products to be received
   let productList = [];
+  //get the query keyword
   const query = document.querySelector("#searchInput").value;
+  //check if there's a query
   if (!query) {
     window.alert("Please type a product name!");
   }
-
+  //Send a GET request to the server with the query keyword
   await fetch(`http://localhost:8080/search?q=${query}`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
   })
     .then((response) => response.json())
     .then((result) => {
-      console.log("Server response:", result);
+      //Set the result to the products list
       productList = result;
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
+  //Call the dispay product method
   displayProducts(productList);
 }
 
 function displayProducts(productList) {
+  //Now it will select the destination of the json products on screen
   const fatherDiv = document.querySelector(".content");
+  //Parse the product list to string to be displayed
   fatherDiv.innerHTML = JSON.stringify(productList, undefined, 4);
 }
